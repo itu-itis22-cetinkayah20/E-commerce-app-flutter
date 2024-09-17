715af2125b811814e4a9f70072a6cac713775b54
Hakan Çetinkaya <cetinkayah20@itu.edu.tr>
2024-09-17 23:23:32 +0300

    Navigation menu is designed.

--- e_commerce/lib/features/authentication/screens/login/widgets/login_form.dart
+++ e_commerce/lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce/features/authentication/screens/password_configuration/forget_password.dart';
 import 'package:e_commerce/features/authentication/screens/signup/signup.dart';
+import 'package:e_commerce/navigation_menu.dart';
 import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:e_commerce/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
@@ -64,7 +65,7 @@ class TLoginForm extends StatelessWidget {
             SizedBox(
               width: double.infinity, // Full width
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () => Get.to(() => const NavigationMenu()),
                 child: const Text(TTexts.signIn),
               ),
             ),
---
+++ e_commerce/lib/navigation_menu.dart
@@ -0,0 +1,60 @@
+import 'package:e_commerce/utils/constants/colors.dart';
+import 'package:e_commerce/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return Scaffold(
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) => controller.selectedIndex.value =
+              index, //selected indexin değerini değiştiriyoruz.
+          backgroundColor: dark ? TColors.black : TColors.white,
+          indicatorColor: dark ? TColors.white.withOpacity(0.1) : TColors.black,
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Iconsax.home),
+              label: 'Home',
+            ),
+            NavigationDestination(
+              icon: Icon(Iconsax.shop),
+              label: 'Shop',
+            ),
+            NavigationDestination(
+              icon: Icon(Iconsax.heart),
+              label: 'Wishlist',
+            ),
+            NavigationDestination(
+              icon: Icon(Iconsax.user),
+              label: 'Profile',
+            ),
+          ],
+        ),
+      ),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
+    );
+  }
+}
+
+class NavigationController extends GetxController {
+  Rx<int> selectedIndex =
+      0.obs; // Navigation menu stateless olduğu için obs kullandık.
+
+  final screens = [
+    Container(color: Colors.red),
+    Container(color: Colors.green),
+    Container(color: Colors.blue),
+    Container(color: Colors.yellow),
+  ];
+}
